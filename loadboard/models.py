60fcc6a93cf2867deed69ce5a490a7cb073ba701
Darryl Lobo <darryl.lobo@outlook.com>
2017-03-05 19:47:26 -0500

    3rd Commit, added some of my own touches to it like a table styled according to bootstrap on the index page

--- loadboard/models.py
+++ loadboard/models.py
@@ -5,23 +5,22 @@ from django.core.urlresolvers import reverse
 #blue print for our database
 
 class Company_table(models.Model):
-    Company = models.CharField(max_length=128)
+    CompanyName = models.CharField(max_length=128)
     Address = models.CharField(max_length=1024)
-    #StartDate = models.DateField()
     CreditScore = models.IntegerField(4)
     Companylogo = models.CharField(max_length=1024,null=True)
 
     def __str__(self):
-        return self.Company
+        return self.CompanyName
 
 class Loadboard_table(models.Model):
-    Company = models.ForeignKey(Company_table, on_delete=models.CASCADE)
+    CompanyName = models.ForeignKey(Company_table, on_delete=models.CASCADE)
     Origin = models.CharField(max_length=128)
     Destination = models.CharField(max_length=128)
     Email = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.Origin + ' ---> ' + self.Destination
+        return self.Origin + ' to ' + self.Destination
 
     def get_absolute_url(self):
         return reverse('loadboard:detail', kwargs={'pk': self.pk})
--- loadboard/views.py
+++ loadboard/views.py
@@ -3,13 +3,11 @@ from .models import Company_table, Loadboard_table
 
 
 class IndexView(generic.ListView):
-    template_name = 'loadboard/index.html'
-    context_object_name = 'all_companies'
+    template_name = 'loadboard/index.html' # the variable contains which html file this view should point to
+    context_object_name = 'all_loadboard' # this variable contains the items from the 'get_queryset(self)' function
 
     def get_queryset(self):
-        return Company_table.objects.all()
-
-
+        return Loadboard_table.objects.all() # queries a list into variable 'context_object_name' variable above
 
 class DetailView(generic.DetailView):
     model = Company_table
